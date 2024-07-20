d5cfadfd849749db6532cbc09227bd1d44559428
Aliaksei Kotsikau <mrtibon@gmail.com>
2024-07-21 00:42:21 +0200

    finish lambda for entire stats

--- lambdas/get_user_statistics_for_game.py
+++ lambdas/get_user_statistics_for_game.py
@@ -17,23 +17,24 @@ class DecimalEncoder(json.JSONEncoder):
             return str(o)
         return super().default(o)
 
+
 def lambda_handler(event, context):
-    # TODO implement
     user = event['user']
     game = event['game']
 
-    userEquals = Key("user").eq(user)
-    gameEquals = Key("game").begins_with(f'{game}#bossDeaths')
+    user_equals = Key("user").eq(user)
+    game_equals = Key("game").begins_with(f'{game}')
 
-    #boss_string = u'{ "Asylum Demon": {"orderNumber": 1, "deathCount": 0}, "Taurus Demon": {"orderNumber": 2, "deathCount": 3}, "Belfry Gargoyles": {"orderNumber": 2, "deathCount": 5}, "Moonlight Butterfly": {"orderNumber": 2, "deathCount": 1}, "Capra Demon": {"orderNumber": 2, "deathCount": 0}, "Gaping Dragon": {"orderNumber": 2, "deathCount": 1}, "Quelaag": {"orderNumber": 2, "deathCount": 2}, "Iron Golem": {"orderNumber": 2, "deathCount": 0}, "Stray Demon": {"orderNumber": 2, "deathCount": 0}, "Priscilla": {"orderNumber": 2, "deathCount": 0}, "Ornstein & Smough": {"orderNumber": 2, "deathCount": 0}, "Gwyndolin": {"orderNumber": 2, "deathCount": 0}, "Sif": {"orderNumber": 2, "deathCount": 0}, "Four Kings": {"orderNumber": 2, "deathCount": 0}, "Pinwheel": {"orderNumber": 2, "deathCount": 0}, "Gravelord Nito": {"orderNumber": 2, "deathCount": 0}, "Seath the Scaleless": {"orderNumber": 2, "deathCount": 0}, "Ceaseless Discharge": {"orderNumber": 2, "deathCount": 0}, "Firesage Demon": {"orderNumber": 2, "deathCount": 0}, "Centipede Demon": {"orderNumber": 2, "deathCount": 0}, "Bed of Chaos": {"orderNumber": 2, "deathCount": 0}, "Sanctuary Guardian": {"orderNumber": 2, "deathCount": 0}, "Artorias": {"orderNumber": 2, "deathCount": 0}, "Manus": {"orderNumber": 2, "deathCount": 0}, "Kalameet": {"orderNumber": 2, "deathCount": 0}, "Gwyn": {"orderNumber": 2, "deathCount": 0}}'
+    response = fitnessSoulsTable.query(
+        KeyConditionExpression=user_equals & game_equals,
+        ProjectionExpression='bosses, exercises, exerciseStats, worldDeathCount'
+    )
 
-    response = fitnessSoulsTable.query(KeyConditionExpression=userEquals & gameEquals )
+    mapped_response = {}
 
-    #fitnessSoulsTable.put_item( Item={'user': user, 'game': f'{game}#bossDeaths', "bosses": json.loads(boss_string)})
+    for item in response['Items']:
+        first_key = next(iter(item))
+        first_value = item[first_key]
+        mapped_response[first_key] = first_value
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response, cls=DecimalEncoder)
-    }
-
-print(lambda_handler({'user': 'kotsial', 'game': 'DarkSouls1'}, None))
+    return json.dumps(mapped_response, cls=DecimalEncoder)
